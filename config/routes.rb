b74f8d5b2c4839a1fa553826bad0fa80d7e9d4f8
John Doe <johndoe@example.com>
2017-08-21 14:39:09 +0000

    201708212338 user follower addition

--- config/routes.rb
+++ config/routes.rb
@@ -20,7 +20,7 @@ Rails.application.routes.draw do
     post :confirm, on: :collection
   end
 
-  resources :users, only: [:index]
+  resources :users, only: [:index,:show]
 
   resources :relationships, only: [:create, :destroy]
 
