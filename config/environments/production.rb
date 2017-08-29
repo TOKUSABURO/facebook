7ad266aacab16d5c8f08282ab73da6b94e4bea25
John Doe <johndoe@example.com>
2017-08-29 08:59:59 +0000

    QQQ

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -7,7 +7,7 @@ Rails.application.configure do
   {
    user_name: ENV['SENDGRID_USERNAME'],
    password: ENV['SENDGRID_PASSWORD'],
-   domain: "https://radiant-eyrie-15168.herokuapp.com/",
+   domain: "heroku.com",
    address: "smtp.sendgrid.net",
    port: 587,
    authentication: :plain,
