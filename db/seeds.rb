a4bfc12c6656debd42e1a3adb11a44c24102a7fc
John Doe <johndoe@example.com>
2017-08-30 07:06:47 +0000

    201708301606 AAA

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,42 @@
+# 50.times do |n|
+# user_id = Random.rand(1..3)
+# title   = Faker::HarryPotter.quote
+# content = Faker::Lorem.paragraph
+#
+# Blog.create!(
+# user_id: user_id,
+# title: title,
+# content: content
+# )
+# end
+# --------
+
 50.times do |n|
 user_id = Random.rand(1..3)
 title   = Faker::HarryPotter.quote
 content = Faker::Lorem.paragraph
+email = Faker::Internet.email
+password = "password"
+provider = Faker::Internet.email
+name = "username"
+@user = User.create(email: email,
+              name: name,
+             password: password,
+             password_confirmation: password,
+             uid: n,
+             provider: provider
+             )
 
-Blog.create!(
-user_id: user_id,
+@blog=Blog.create!(
+user_id: @user.id,
 title: title,
 content: content
 )
+
+Comment.create!(
+user_id: @user.id,
+blog_id: @blog.id,
+content: content
+)
+
 end
